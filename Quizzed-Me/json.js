22d86265a2c97dc92153bfee11c82d44b5e45db9
Rishika Bhanushali <40559416+amateurRB@users.noreply.github.com>
2020-07-12 23:12:07 +0530

    Add files via upload

---
+++ Quizzed-Me/json.js
@@ -0,0 +1,12 @@
+data = {"quizcontent": [
+    {"question": "DisneyWorld","image": "meta/intr1.png",  "correct": 2, "a1":"You're never too old for Disney", "a2":"The happiest place on earth.","a3":"Heavens fall here","a4":"It was all started by a mouse!"},
+    {"question": "Lays","image": "meta/intr2.jpg", "correct": 3, "a1":"Not just any chips", "a2":"Feel the crispiness","a3":"Betcha can't eat just one.","a4":"Taste the best!"},
+	{"question": "Old Spice", "image": "meta/intr3.jpg", "correct": 3, "a1":"No bottles to break – just hearts. ...    ", "a2":"The fragrance larger than life.    ","a3":"Believe In Your Smellf.","a4":"Life without passion is unforgivable    "},
+	{"question": "Pepsi","image": "meta/intr4.jpg", "correct": 1, "a1":"The taste of a new generation.", "a2":"Pagalpanti bhi zaroori hai","a3":"dil maange more","a4":"Pyaas Badhao"},
+    {"question": "Fisher Price","image": "meta/intr5.jpg",  "correct": 4, "a1":"Back to School", "a2":"Twice the muff, half the puff","a3":"Your way to awesomeness","a4":"Play. Laugh. Grow."},
+    {"question": "Jaguar","image": "meta/intr6.jpg","correct": 2, "a1":"Power, beauty and soul", "a2":"Don’t dream it. Drive it.","a3":"The new class of world class","a4":"Excellence for Everyone. Find New Roads"},
+	{"question": "Coca-Cola", "image": "meta/intr7.jpg","correct": 1, "a1":"Open happiness", "a2":"Life tastes good","a3":"The coke side of life","a4":"Life Begins here"},
+	{"question": "Adidas","image": "meta/intr8.jpg", "correct": 4, "a1":"Evertything is possible", "a2":"Nothing is impossible","a3":"Impossible is nothing","a4":"All in or nothing"},
+    ],
+	
+};
